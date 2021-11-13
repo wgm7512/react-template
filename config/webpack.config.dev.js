1908c3ea0f9f4254e7930e6d540e7d950720262d
ThroughTheNight <jmethods@163.com>
2021-11-13 17:15:22 +0800

    add: 增加 css

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -3,6 +3,7 @@ const webpackConfigBase = require('./webpack.config.base.js');
 const path = require('path');
 
 const webpackDevConfig = {
+  mode: 'development', // 如果没有手动的设置 NODE_ENV ，在执行 webpack 后，会将 mode 同步到 NODE_ENV 中
   devServer: {
     port: 3304,
     host: 'localhost',
